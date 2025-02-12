e21851b735390f2fdd8a1948b8e431e864897b40
MVS Koushik <medurivskoushik@gmail.com>
2025-02-12 21:53:46 +0530

    Search Box Plecement

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './components/Header';
 import SignIn from './components/SignIn';
+import Home from './components/Home';
 import "./App.css";
 
 const App = () => {
@@ -19,12 +20,5 @@ const App = () => {
 };
 
 // Home component for the opening page
-const Home = () => {
-  return (
-    <div className="home">
-      <h1>Welcome to CityConnectApp</h1>
-    </div>
-  );
-};
 
 export default App;
