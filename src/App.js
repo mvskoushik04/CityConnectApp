acd6e7b06b41986f5f32a9ad22f03b8fbd46704b
MVS Koushik <medurivskoushik@gmail.com>
2025-02-15 23:41:17 +0530

    Search Routes Adding

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './components/Header';
 import SignIn from './components/SignIn';
 import Home from './components/Home';
+import Search from './components/Search';
 import "./App.css";
 
 const App = () => {
@@ -13,6 +14,7 @@ const App = () => {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/signin" element={<SignIn />} />
+          <Route path="/search" element={<Search />} /> {/* Add the Search route */}
         </Routes>
       </div>
     </Router>
