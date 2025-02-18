dedfa22c369c02b9a7413dd87679f3c366648433
MVS Koushik <medurivskoushik@gmail.com>
2025-02-18 23:25:43 +0530

    Launching a server in backend

---
+++ cityconnect-backend/Server.js
@@ -0,0 +1,9 @@
+// server.js
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
